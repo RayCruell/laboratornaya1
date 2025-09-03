6eec630a1e3c596f2370375917fae6e4adf7f695
SAPR <SAPR@PC-NXT>
2025-09-03 15:21:26 +0700

    Ъуъ

--- laboratornaya1/MainWindow.xaml.cs
+++ laboratornaya1/MainWindow.xaml.cs
@@ -10,10 +10,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace laboratornaya1
-{
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+{ 
     public partial class MainWindow : Window
     {
         public MainWindow()
